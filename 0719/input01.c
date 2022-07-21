0d48b39fcd75e37c2b39e996cee5c36384fab683
오병욱 <obw@192.168.nate.com>
2022-07-21 19:13:54 +0900

    first commit

---
+++ 0719/input01.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char str1[10];
+    printf("input string:");
+    gets(str1);
+    printf("input string is %s",str1);
+}
---
+++ 0719/prac01.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+    char ch1;
+    printf("input charter:");
+    scanf("%c",&ch1);
+    printf("input   charter is  %c.", ch1);
+}
